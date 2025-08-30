c8136a905dfcd40b7167d81476a0a7aa0143563e
TejeshReddy999 <mallampatitejesh@gmail.com>
2025-08-30 18:46:04 -0500

    Removed Testing and optimized code

---
+++ src/main/java/com/mvr/authservice/GlobalExceptionHandler.java
@@ -0,0 +1,11 @@
+package com.mvr.authservice;
+
+import org.springframework.http.InvalidMediaTypeException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    public ResponseEntity<Error> handleException(InvalidMediaTypeException in)
+}
